6993d916eb3eed58140a43f30570e356a782817a
YouKangChui <104844105+YouKangChui@users.noreply.github.com>
2023-01-12 17:11:40 +0800

    Update main.go

--- main.go
+++ main.go
@@ -50,12 +50,6 @@ func init() {
 }
 
 func main() {
-http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
-	})
-
-	http.ListenAndServe(":80", nil)
-
 	token, err := getToken(tgtoken)
 	if err != nil {
 		log.Fatalln(err)
